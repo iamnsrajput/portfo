c1504bc7d194b36382f91d294ef5709c1f0be388
Nilesh <imnsrajput@gmail.com>
2020-06-16 13:49:20 +0530

    Portfolio Website with Python Flask

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect, request
+import csv
+
+app = Flask(__name__)
+print(app)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return "Sorry Your Data is not saved to Database"
+    else:
+        return 'Sorry'
